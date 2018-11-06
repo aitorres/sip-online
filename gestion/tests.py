c518698b3d6f3bf4b5654ccf6a483973060215a5
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-11-05 20:33:10 -0400

    Se agregan pruebas para verificar que el Profesor puede guardar nombre y apellido; en este momento, las pruebas fallan porque Profesor no tiene atributos.

--- gestion/tests.py
+++ gestion/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
-# Create your tests here.
+from gestion.models import Profesor
+
+class ProfesorModelTest(TestCase):
+
+    def test_nombre_profesor(self):
+        """
+        PRUEBA 1. Se verifica que se guarde el nombre del profesor
+        en la entidad, y que luego corresponda con el nombre guardado.
+
+        PRIMERA CORRIDA: Falla porque el modelo profesor no tiene atributos.
+        """
+
+        profesor = Profesor(nombre="Andrés")
+        self.assertEqual(profesor.nombre, "Andrés")
+
+    def test_apellido_profesor(self):
+        """
+        PRUEBA 2. Se verifica que se guarde el apellido del profesor
+        en la entidad, y que luego corresponda con el apellido guardado.
+
+        PRIMERA CORRIDA: Falla porque el modelo profesor no tiene atributos.
+        """
+
+        profesor = Profesor(apellido="Medina")
+        self.assertEqual(profesor.apellido, "Medina")
