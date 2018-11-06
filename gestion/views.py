6356d92f6e41d635c17e4096c00f34da1779eb0f
mgmdi <14-10444@usb.ve>
2018-11-05 20:27:52 -0400

    Se agrego:
    	- Clase eliminarProfesor como controlador para eliminar profesor. Dentro de la clase se definio:
    			- template_name
    			- Nombre del modelo correspondiente

--- gestion/views.py
+++ gestion/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import DeleteView
+from gestion.models import Profesor
 
 def index(request):
     return render(request, 'template.html')
+
+class eliminarProfesor(DeleteView):
+    template_name = 'profesores/eliminar.html'
+    model = Profesor
