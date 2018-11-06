4e9f1008f2747f6c7e379c483c9079f1a25c998a
Julio Cesar Perez Duran <perezduranjulio@gmail.com>
2018-11-05 20:59:09 -0400

    se extendio DetailView para detalles de profesor

--- gestion/views.py
+++ gestion/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.detail import DetailView
+from gestion.models import Profesor
 
 def index(request):
     return render(request, 'template.html')
+
+class VerProfesor(DetailView):
+    
+    template_name = 'profesores/ver.html'
+    model = Profesor
+    
+    def get_context_data(self, **kwargs):
+        context = super(VerProfesorlView, self).get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+    def get_context_object_name(self):
+        return "profesor"
