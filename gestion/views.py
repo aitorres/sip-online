24966d1417e77d252baf2f3ad521c265fde6290d
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-11-06 00:35:29 -0400

    Corrige import y método para devolver nombre de objeto

--- gestion/views.py
+++ gestion/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic.edit import ListView
+from django.views.generic import ListView
 from gestion.models import Profesor
 
 def index(request):
     return render(request, 'template.html')
 
 class ListarProfesores(ListView):
-	template_name = 'profesores/listar.html'
-	model = Profesor
-
-	def get_context_object_name(self):
-		return("profesores")
+    """
+    Controlador que muestra una lista en tabla de todos los
+    profesores.
+    """
+    template_name = 'profesores/listar.html'
+    model = Profesor
+    context_object_name = "profesores"
