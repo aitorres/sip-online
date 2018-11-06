76579058d9ad0266b7f08e383fea3fa3d6d57029
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-11-06 00:27:21 -0400

    Corrige y extiende controlador de Dashboard

--- gestion/views.py
+++ gestion/views.py
@@ -2,19 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 
-from gestion.models import Profesor
+from gestion.models import Profesor, Asignatura, Departamento
 
 class Dashboard(generic.TemplateView):
     """
     Controlador para el dashboard principal de la aplicación.
-    Renderiza la plantilla de manera estática.
+    Renderiza la plantilla incluyendo contadores dinámicos
+    de algunas entidades de la base de datos.
     """
 
     template_name = "dashboard.html"
 
     def get_context_data(self, *args, **kwargs):
+        """
+        Devuelve el diccionario de contexto básico para la plantilla
+        a renderizar. Incluye algunos contadores de datos.
+        """
         context = super(Dashboard, self).get_context_data(*args, **kwargs)
+
+        # Contamos la cantidad de cada elemento de la base de datos
+        # para reportarlo en el Dashboard
         context['profesores'] = Profesor.objects.all().count()
-        #context['asignaturas'] = Asignatura.objects.all().count()
-        #context['departamento'] = Departamento.objects.all().count()
+        context['asignaturas'] = Asignatura.objects.all().count()
+        context['departamento'] = Departamento.objects.all().count()
+
         return context
