81e64508724750415fa18653917a9ff7d1d12cd9
14-10526 <14-10526@usb.ve>
2018-11-05 20:24:36 -0400

    Se agrego  views y url

--- gestion/views.py
+++ gestion/views.py
@@ -1,5 +1,13 @@
+from django.views.generic.edit import UpdateView
 from django.shortcuts import render
 from django.http import HttpResponse
+from gestion.models import Profesor 
 
 def index(request):
     return render(request, 'template.html')
+
+class EditarProfesor(UpdateView):
+	model = Profesor
+	fields = ['nombre','materias','Estatus','Disponibilidad']
+	template_name_suffix = 'profesores/editar.html'
+		
