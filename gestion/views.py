9c77ae2b101afde2ce29a3c817a99906f4de3c97
Javier <javivasv@hotmail.com>
2018-11-05 23:35:14 -0400

    Extendida vista ListView y creada la vista ListarProfesores

--- gestion/views.py
+++ gestion/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import ListView
+from gestion.models import Profesor
 
 def index(request):
     return render(request, 'template.html')
+
+class ListarProfesores(ListView):
+	template_name = 'profesores/listar.html'
+	model = Profesor
