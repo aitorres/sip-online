f7e38bbe79a4e2d83da276cc8569024e91f3fb9d
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-11-05 21:56:07 -0400

    Agrega una versión sencilla del dashboard. Falta modificar la tarjeta de perfil de usuario.

--- gestion/views.py
+++ gestion/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
-def index(request):
-    return render(request, 'template.html')
+from gestion.models import Profesor
+
+class Dashboard(generic.TemplateView):
+    """
+    Controlador para el dashboard principal de la aplicación.
+    Renderiza la plantilla de manera estática.
+    """
+
+    template_name = "dashboard.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(Dashboard, self).get_context_data(*args, **kwargs)
+        context['profesores'] = Profesor.objects.all().count()
+        #context['asignaturas'] = Asignatura.objects.all().count()
+        #context['departamento'] = Departamento.objects.all().count()
+        return context
