8fb2514e735f647593770fc01f2fbf8be907cf12
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-12-05 20:55:21 -0400

    Extiende y corrige modelo OfertaTrimestral

--- gestion/models.py
+++ gestion/models.py
@@ -277,34 +277,76 @@ class Disponibilidad(models.Model):
 
 class OfertaTrimestral(models.Model):
     """
-    Modelo que representa la oferta 
-    trimestra de un DEPARTAMENTO con los datos sobre:
+    Modelo que representa la oferta trimestral de un DEPARTAMENTO con los datos sobre:
     El codigo de la forma XX00, donde la X representan la letra del trimestre por ejemplo EM
-    y los 00 representan el a;o, por ejemplo 18. Teniendo asi como trimestre EM18.
-    El campo estado, de tipo booleano, indica si la oferta es final o no.
+    y los 00 representan el año, por ejemplo 18. Teniendo asi como trimestre EM18.
+    El campo es_final, de tipo booleano, indica si la oferta es final o no.
     """
-    trimestre = models.CharField(max_length=4)
-    estado = models.BooleanField(default=False)
-    asignacion_profesoral = models.ForeignKey('AsignacionProfesoral')
+
+    trimestre = models.CharField(max_length=4, unique=True)
+    es_final = models.BooleanField(default=False)
+
+    def nombre_completo(self):
+        """
+        Retorna el nombre completo de un trimestre reconstruyéndolo a partir
+        de su código.
+        """
+
+        NOMBRES = {
+            'EM': "Enero  Marzo",
+            'AJ': "Abril - Julio",
+            'JA': "Julio - Agosto",
+            'SD': "Septiembre - Diciembre"
+        }
+
+        # Obtenemos el trimestre a partir del código de período
+        codigo_periodo = self.trimestre[0:2]
+        nombre_trimestre = NOMBRES[codigo_periodo]
+
+        # Obtenemos el año, entre 2000 y 2099
+        ano = "20" + self.trimestre[2:]
+
+        nombre_completo = "%s %s" % (nombre_trimestre, ano)
+        return nombre_completo
+
+    def estado(self):
+        """
+        Retorna una representación en cadena de caracteres del estado
+        de la oferta trimestral.
+        """
+
+        estado = "final" if self.es_final else "preliminar"
+        return estado
+
+    def __str__(self):
+        """
+        Retorna una representación como cadena de caracteres de la oferta trimestral.
+        """
+
+        return self.nombre_completo()
+
 
 class AsignacionProfesoral(models.Model):
     """
     Modelo que representa una asignación de una asignatura en un trimestre a un profesor de la USB.
-    Incluye la oferta trimestral, el profesor asociado, la asignatura asociada,un campo booleano que indica si la 
+    Incluye la oferta trimestral, el profesor asociado, la asignatura asociada, un campo booleano que indica si la
     asignacion es final y el campo tipo que indica la modalidad de la materia
     """
-    oferta_trimestral = models.ForeignKey('OfertaTrimestral')
-    profesor = models.ForeignKey('Profesor')
-    asignatura = models.ForeignKey('Asignatura')
-    final = models.BooleanField(default=False)
+
     TEORIA = 'TEO'
     PRACTICA = 'PRA'
     OTRO ='OTR'
+
     TIPO_CHOICES = (
         (TEORIA, 'Teoría'),
         (PRACTICA, 'Práctica'),
         (OTRO,'Otro'),
     )
+
+    oferta_trimestral = models.ForeignKey('OfertaTrimestral')
+    profesor = models.ForeignKey('Profesor')
+    asignatura = models.ForeignKey('Asignatura')
+    es_final = models.BooleanField(default=False)
     tipo = models.CharField(
         max_length=3,
         choices=TIPO_CHOICES,
