eba7dc3d0ccf84548859b1570ba9def38411d9c2
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-12-23 02:29:35 -0400

    Instala addon de chat para mensajes

--- sip/settings.py
+++ sip/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gestion',
-    'django_nose'
+    'django_nose',
+    'conversate'
 ]
 
 # Django-nose Measuring coverage
